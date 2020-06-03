88fda33b6346c5c86fb72ce45d820018cf87b004
taolei <tl@chiebot.com>
2020-06-03 12:40:42 +0800

    删除未使用链接

--- spring-security/helloworld/src/main/java/com/neotao/springsecurity/controller/MainController.java
+++ spring-security/helloworld/src/main/java/com/neotao/springsecurity/controller/MainController.java
@@ -20,11 +20,4 @@ public class MainController {
     public String login() {
         return "login";
     }
-
-    @GetMapping("/login-error")
-    public String loginError() {
-        return "login";
-    }
-
-
 }
